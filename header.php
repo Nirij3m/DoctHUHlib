bd492265bd0c72e2d536de201507ae9661042e1d
Nirina Macon <nirina.macon@isen-ouest.yncrea.fr>
2023-12-05 17:21:28 +0100

    Header creation

---
+++ header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="src/css/header.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
+    <script src="https://kit.fontawesome.com/5b8b37978c.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row flex-nowrap">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="accueil.php" class="linkTitle d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span class="fs-5 d-none d-sm-inline title">Doct'HUH'lib</span>
+                </a>
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li class="nav-item">
+                        <a href="accueil.php" class="link align-middle px-0">
+                            <i class="fa-solid fa-house"></i>
+                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Accueil</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="rendezvous.php" class="link px-0 align-middle">
+                            <i class="fa-solid fa-calendar-days"></i>
+                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Rendez-vous</span></a>
+                    </li>
+
+                </ul>
+                <hr>
+                <?php 
+                    $session = session_id();
+                    if ($session == "") // User is not connected 
+                    {
+                        echo '
+                                                <div class="dropdown pb-4">
+                                                <a href="/login.php" class="d-flex align-items-center text-white text-decoration-none>
+                                                    <i class="fa-solid fa-right-to-bracket"></i>
+                                                    <span class="d-none d-sm-inline mx-1">Se connecter</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                        ';
+                    }
+                    else {
+                        echo '
+                                            <div class="dropdown pb-4">
+                                            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                                <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                                                <span class="d-none d-sm-inline mx-1">Hello</span>
+                                            </a>
+                                            <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                                                <li><a class="dropdown-item" href="#">Se déconnecter</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>';
+                    }
+                ?>
+         
+        <div class="corpse"> <!-- Content area-->
+            <div id="topHeader">
+                <p id="namePage" class="fs-5 d-none d-sm-inline title">Accueil</p>
+            </div>
+
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
---
+++ ressources/CTDoctisen.sql
@@ -0,0 +1,106 @@
+DROP TABLE IF EXISTS Speciality CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Timeslot CASCADE;
+DROP TABLE IF EXISTS Meeting CASCADE;
+DROP TABLE IF EXISTS Place CASCADE;
+DROP TABLE IF EXISTS City CASCADE;
+DROP TABLE IF EXISTS works CASCADE;
+
+------------------------------------------------------------
+-- Table: Speciality
+------------------------------------------------------------
+CREATE TABLE Speciality(
+	id     SERIAL NOT NULL ,
+	type   VARCHAR (128) NOT NULL  ,
+	CONSTRAINT Speciality_PK PRIMARY KEY (id)
+);
+
+
+------------------------------------------------------------
+-- Table: User
+------------------------------------------------------------
+CREATE TABLE Users(
+	id              SERIAL NOT NULL ,
+	name            VARCHAR (128) NOT NULL ,
+	surname         VARCHAR (128) NOT NULL ,
+	phone           CHAR (10)  NOT NULL ,
+	mail            VARCHAR (128) NOT NULL ,
+	password        VARCHAR (60) NOT NULL ,
+	id_Speciality   INT    ,
+	CONSTRAINT User_PK PRIMARY KEY (id)
+
+	,CONSTRAINT User_Speciality_FK FOREIGN KEY (id_Speciality) REFERENCES Speciality(id)
+);
+
+
+------------------------------------------------------------
+-- Table: City
+------------------------------------------------------------
+CREATE TABLE City(
+	code_insee    INT  NOT NULL ,
+	city          VARCHAR (128) NOT NULL ,
+	code_postal   INT  NOT NULL  ,
+	CONSTRAINT City_PK PRIMARY KEY (code_insee)
+);
+
+
+------------------------------------------------------------
+-- Table: Place
+------------------------------------------------------------
+CREATE TABLE Place(
+	id           SERIAL NOT NULL ,
+	name         VARCHAR (128) NOT NULL ,
+	num_street   INT  NOT NULL ,
+	street       VARCHAR (128) NOT NULL ,
+	code_insee   INT  NOT NULL  ,
+	CONSTRAINT Place_PK PRIMARY KEY (id)
+
+	,CONSTRAINT Place_City_FK FOREIGN KEY (code_insee) REFERENCES City(code_insee)
+);
+
+
+------------------------------------------------------------
+-- Table: Meeting
+------------------------------------------------------------
+CREATE TABLE Meeting(
+	id                 SERIAL NOT NULL ,
+	beginning          DATE  NOT NULL ,
+	ending                DATE  NOT NULL ,
+	id_Place           INT  NOT NULL ,
+	id_User            INT  NOT NULL ,
+	id_User_Asks_for   INT  NOT NULL  ,
+	CONSTRAINT Meeting_PK PRIMARY KEY (id)
+
+	,CONSTRAINT Meeting_Place_FK FOREIGN KEY (id_Place) REFERENCES Place(id)
+	,CONSTRAINT Meeting_User0_FK FOREIGN KEY (id_User) REFERENCES Users(id)
+	,CONSTRAINT Meeting_User1_FK FOREIGN KEY (id_User_Asks_for) REFERENCES Users(id)
+);
+
+
+------------------------------------------------------------
+-- Table: Timeslot
+------------------------------------------------------------
+CREATE TABLE Timeslot(
+	id          SERIAL NOT NULL ,
+	beginning   DATE  NOT NULL ,
+	ending         DATE  NOT NULL ,
+	id_User     INT  NOT NULL  ,
+	CONSTRAINT Timeslot_PK PRIMARY KEY (id)
+
+	,CONSTRAINT Timeslot_User_FK FOREIGN KEY (id_User) REFERENCES Users(id)
+);
+
+
+------------------------------------------------------------
+-- Table: works
+------------------------------------------------------------
+CREATE TABLE works(
+	id        INT  NOT NULL ,
+	id_User   INT  NOT NULL  ,
+	CONSTRAINT works_PK PRIMARY KEY (id,id_User)
+
+	,CONSTRAINT works_Place_FK FOREIGN KEY (id) REFERENCES Place(id)
+	,CONSTRAINT works_User0_FK FOREIGN KEY (id_User) REFERENCES Users(id)
+);
+
+
